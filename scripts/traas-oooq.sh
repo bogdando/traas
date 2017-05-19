49a1b6fcde79afd46880a4e37546bf64638db4a1
Bogdan Dobrelya <bogdando@mail.ru>
2017-05-19 11:49:59 +0200

    Split overcloud nodes into controller/computes
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- scripts/traas-oooq.sh
+++ scripts/traas-oooq.sh
@@ -14,12 +14,14 @@ cd $TRIPLEO_ROOT
 export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 export PRIMARY_NODE_IP=${PRIMARY_NODE_IP:-""}
 export SUB_NODE_IPS=${SUB_NODE_IPS:-""}
-export SSH_OPTIONS=${SSH_OPTIONS:-'-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=Verbose -o PasswordAuthentication=no -o ConnectionAttempts=32 -i ~/.ssh/id_rsa'}
-export TRIPLEO_CI_REMOTE=${TRIPLEO_CI_REMOTE:-https://git.openstack.org/openstack-infra/tripleo-ci}
-export TRIPLEO_CI_BRANCH=${TRIPLEO_CI_BRANCH:-master}
+#export SSH_OPTIONS=${SSH_OPTIONS:-'-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=Verbose -o PasswordAuthentication=no -o ConnectionAttempts=32 -i ~/.ssh/id_rsa'}
+#export TRIPLEO_CI_REMOTE=${TRIPLEO_CI_REMOTE:-https://git.openstack.org/openstack-infra/tripleo-ci}
+#export TRIPLEO_CI_BRANCH=${TRIPLEO_CI_BRANCH:-master}
 
-rpm -q git || sudo yum -y install git
+#rpm -q git || sudo yum -y install git
 
-[ -d tripleo-ci ] || git clone -b $TRIPLEO_CI_BRANCH $TRIPLEO_CI_REMOTE
-$TRIPLEO_ROOT/tripleo-ci/scripts/tripleo.sh --setup-nodepool-files
+#[ -d tripleo-ci ] || git clone -b $TRIPLEO_CI_BRANCH $TRIPLEO_CI_REMOTE
+echo "$PRIMARY_NODE_IP" > /tmp/PRIMARY_NODE_IP
+echo "$SUB_NODE_IPS" > /tmp/SUB_NODE_IPS
+#$TRIPLEO_ROOT/tripleo-ci/scripts/tripleo.sh --setup-nodepool-files
 #TODO(bogdando) invoke ansible-playbook commands wrapped with oooq-warp
